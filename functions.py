2145e9aca3f88399db231cc56d9df6e890debc10
Christopher Yeaw <tyeaw@Tammy.local>
2023-09-24 15:31:39 -0400

    Initial commit

---
+++ functions.py
@@ -0,0 +1,18 @@
+FILEPATH = 'todos.txt'
+
+
+def get_todos(filepath=FILEPATH):
+    """
+    Read a text file and return the list of to-do items.
+    """
+    with open(filepath, 'r') as file:
+        todos_local = file.readlines()
+    return todos_local
+
+
+def write_to_file(data, filepath=FILEPATH):
+    """
+    Write the to-do items list in the text file.
+    """
+    with open(filepath, 'w') as file:
+        file.writelines(data)
